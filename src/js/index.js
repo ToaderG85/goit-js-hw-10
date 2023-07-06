6a3140c7fcb610e8a561ae70bee9dd8cf36fb491
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-07-06 11:48:07 +0300

    update x

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 import api from "./cat-api.js";
+// import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
 
 const select = document.querySelector(".breed-select");
+select.classList.add("hidden");
+// new SlimSelect({
+//     select: select,
+//     settings: {
+//         alwaysOpen: false,
+//         openPosition: "down",
+//         showSearch: false,
+//         closeOnSelect: true,
+//     }
+// });
+
+const loader = document.querySelector(".loader");
+const errorMessage = document.querySelector(".error");
+errorMessage.classList.add("hidden");
+const catInfo = document.querySelector(".cat-info");
 
 api.fetchBreeds()
     .then(optionsList => {
@@ -8,28 +25,37 @@ api.fetchBreeds()
         optionsList.forEach(element => {
            let option = document.createElement("option");
            option.setAttribute("value", element.id);
+           option.classList.add("option");
            select.append(option);
            option.textContent = element.name;
         });
+        
+        select.classList.remove("hidden");
+        loader.classList.add("hidden");
     })
     .catch(err => {
-        console.log(err)
+        loader.classList.add("hidden");
+        errorMessage.classList.remove("hidden");
     });
 
 select.addEventListener("change", (event) => {
+    loader.classList.remove("hidden");
+    catInfo.classList.add("hidden");
+    errorMessage.classList.add("hidden");
     api.fetchCatByBreed(event.currentTarget.value)
     .then(result => {
-        console.log(result);
-        const catInfo = document.querySelector(".cat-info");
-        catInfo.innerHTML = `<img src="${result[0].url}" width="550px" class="cat-image" alt="">
-        <h2 class="breed-name">${result[0].breeds[0].name}</h2>
+        console.log(result);        
+        catInfo.innerHTML = `<div class="cat-data"><img src="${result[0].url}" width="550px" class="cat-image" alt="">
+        <div class="breed-info"><h2 class="breed-name">${result[0].breeds[0].name}</h2>
         <p class="breed-description">${result[0].breeds[0].description}</p>
-        <p class="breed-temperament"><span>Temperament</span>${result[0].breeds[0].temperament}</p>`;  
+        <p class="breed-temperament"><span>Temperament: </span>${result[0].breeds[0].temperament}</p></div></div>`;  
+        loader.classList.add("hidden");
+        catInfo.classList.remove("hidden");
+       
     })
     .catch (err => {
-        console.log(err)
+        loader.classList.add("hidden");
+        errorMessage.classList.remove("hidden");
     });
 });
 
-const loader = document.querySelector(".loader");
-loader.classList.add("hidden");
